fb172a1d45165ff0ac4e3ce492117c7d198a7e00
Лонин Даниил Валерьевич <lonindaniil@gmail.com>
2020-12-02 19:28:35 +0300

    a lot of fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Swiper, {Pagination} from 'swiper';
 import '../scss/style.scss';
 
+const body = document.body;
 const headerButtonMenu = document.querySelector('.header__button-menu');
 const buttonCloseMenu = document.querySelector('.button--close');
 const menuWrapper = document.querySelector('.menu-wrapper');
@@ -16,9 +17,15 @@ const menuLinks = document.querySelectorAll('.menu__link');
 const aboutButton = document.querySelector('.about__button');
 const aboutText = document.querySelectorAll('.about__text');
 const brandsList = document.querySelector('.brands__list'); 
+const brandsItem = brandsList.querySelector('.brands__item');
 const brandsButton = document.querySelector('.brands__button');
 const servicesList = document.querySelector('.services__list');
+const servicesItem = servicesList.querySelector('.services__item')
 const servicesButton = document.querySelector('.services__button');
+
+console.log(servicesItem.offsetHeight);
+ 
+
 
 Swiper.use([Pagination]);
 
@@ -63,6 +70,10 @@ if (window.innerWidth >= 575) {
     swiperBrands.destroy();
     swiperServices.destroy();
     swiperPrice.destroy();
+
+    brandsList.style.height = `${brandsItem.offsetHeight * 2 + 16}px`;
+    servicesList.style.height = `${servicesItem.offsetHeight + 16}px`;
+
 }
 
 menuLinks.forEach(link => {
@@ -95,11 +106,13 @@ aboutButton.addEventListener('click', function() {
 
 brandsButton.addEventListener('click', function() {
     if (!brandsList.classList.contains('brands__list--unfolded')) {
-        brandsList.classList.add('brands__list--unfolded');
+        brandsList.style.height = '100%';
+        brandsList.classList.add('brands__list--unfolded')
         brandsButton.textContent = 'Скрыть'
     } else {
-        brandsList.classList.remove('brands__list--unfolded');
+        brandsList.style.height = `${brandsItem.offsetHeight * 2 + 16}px`;
         brandsButton.textContent = 'Показать все'
+        brandsList.classList.remove('brands__list--unfolded')
     }
 
     brandsButton.classList.toggle('rotated');
@@ -107,11 +120,13 @@ brandsButton.addEventListener('click', function() {
 
 servicesButton.addEventListener('click', function() {
     if (!servicesList.classList.contains('services__list--unfolded')) {
+        servicesList.style.height = '100%';
         servicesList.classList.add('services__list--unfolded');
         servicesButton.textContent = 'Скрыть'
     } else {
-        servicesList.classList.remove('services__list--unfolded');
-        servicesButton.textContent = 'Показать все'
+        servicesList.style.height = `${servicesItem.offsetHeight + 16}px`;
+        servicesButton.textContent = 'Показать все';
+        servicesList.classList.remove('services__list--unfolded')
     }
 
     servicesButton.classList.toggle('rotated');
@@ -120,6 +135,11 @@ servicesButton.addEventListener('click', function() {
 
 function showMobileMenu() {
     menuWrapper.classList.toggle('menu-wrapper--opened');
+    disableScroll();
+}
+
+function disableScroll() {
+   body.classList.toggle('no-scroll');
 }
 
 headerButtonMenu.addEventListener('click', showMobileMenu);
@@ -135,12 +155,19 @@ buttonsOpenFormFeedback.forEach(button => {
         const top = window.pageYOffset;
         feedbackWrapper.style.top = `${top}px`;
         feedbackWrapper.classList.toggle('form-wrapper--opened');
+        if(!menuWrapper.classList.contains('menu-wrapper--opened')) {
+            disableScroll();
+        }
     })
 })
 
 feedbackWrapper.addEventListener('click', function(event) {
     if(event.target === buttonCloseFormFeedback || event.target === feedbackWrapper) {
         feedbackWrapper.classList.toggle('form-wrapper--opened');
+    }
+
+    if(!menuWrapper.classList.contains('menu-wrapper--opened')) {
+        disableScroll();
     }
 })
 
@@ -149,12 +176,19 @@ buttonsOpenFormCall.forEach(button => {
         const top = window.pageYOffset;
         callWrapper.style.top = `${top}px`;
         callWrapper.classList.toggle('form-wrapper--opened');
+        if(!menuWrapper.classList.contains('menu-wrapper--opened')) {
+            disableScroll();
+        }
     })
 })
     
 callWrapper.addEventListener('click', function(event) {
     if(event.target === buttonCloseFormCall || event.target === callWrapper)
     callWrapper.classList.toggle('form-wrapper--opened');
+
+    if(!menuWrapper.classList.contains('menu-wrapper--opened')) {
+        disableScroll();
+    }
 })
 
 
